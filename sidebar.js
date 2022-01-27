0fac32795c419c1e3b3a81919deac635323a12be
mychiara <85479169+mychiara@users.noreply.github.com>
2022-01-27 21:48:18 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,8 @@
-atOptions = {
+	atOptions = {
 		'key' : 'c5ba7a3b515b1eb51835f2066ed18bdd',
 		'format' : 'iframe',
 		'height' : 600,
 		'width' : 160,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/c5ba7a3b515b1eb51835f2066ed18bdd/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/c5ba7a3b515b1eb51835f2066ed18bdd/invoke.js"></scr' + 'ipt>');
