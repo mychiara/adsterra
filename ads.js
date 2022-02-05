19b19de7eabab87f400607af8f1cc844743ca4cc
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-06 00:06:07 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,13 @@
-atOptions = { 
-    'key' : '0c36e62040e13f23ed831c26d97931ec', 
-    'format' : 'iframe', 
-    'height' : 250, 
-    'width' : 300, 
-    'params' : {} 
-  }; 
-  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/0c36e62040e13f23ed831c26d97931ec/invoke.js"></scr' + 'ipt>');
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://presumptuousfunnelinsight.com/z8s2mtu115?key=68c2c56732cf23a7b4915eb49cf01442', '_blank');
+	});
+//]]>
