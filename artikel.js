2fb4ea53da1e3ec38a7a74c33f69344f2aee33c6
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-07 12:00:16 +0800

    Update artikel.js

--- artikel.js
+++ artikel.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '0c36e62040e13f23ed831c26d97931ec',
+		'key' : '98997cebcc422dcf318d719442cb1a7d',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/0c36e62040e13f23ed831c26d97931ec/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/98997cebcc422dcf318d719442cb1a7d/invoke.js"></scr' + 'ipt>');
