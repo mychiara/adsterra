aad4ba508e5331709ddabf632e89d99b7c675a7b
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-12 18:09:33 +0800

    Update head.js

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '0c36e62040e13f23ed831c26d97931ec',
+		'key' : '6abd20331a30cbdfe9589653b336f61b',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 90,
+		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/0c36e62040e13f23ed831c26d97931ec/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/6abd20331a30cbdfe9589653b336f61b/invoke.js"></scr' + 'ipt>');
