7b034be69ed0b416d08bbb4ca6e7b0070ce2d709
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-12 18:19:51 +0800

    Update artikel.js

--- artikel.js
+++ artikel.js
@@ -6,3 +6,12 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/98997cebcc422dcf318d719442cb1a7d/invoke.js"></scr' + 'ipt>');
+
+	atOptions = {
+		'key' : '2176f8ab295f45079e31772558216272',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/2176f8ab295f45079e31772558216272/invoke.js"></scr' + 'ipt>');
