8997e70649546aadb81ff676f6db6bce63bfae21
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-13 21:23:43 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,1 +1,10 @@
-<meta name="google-site-verification" content="mf6bW9HWpOxdKj2z2oZnygW0BBmnnc1m1FOwxkcLv6k" />
+export default {
+  head: {
+      {
+        name: 'google-site-verification',
+        content: '<meta name="google-site-verification" content="mf6bW9HWpOxdKj2z2oZnygW0BBmnnc1m1FOwxkcLv6k" />'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  }
+}
