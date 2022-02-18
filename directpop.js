52fd5d5813ddfe01d3ea10794135e53609bece32
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-18 08:13:54 +0800

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,6 +8,6 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://www.trustedcpmrevenue.com/jsutw9n4?key=fc13c94f072d24b5a57b8f81c11d12be', '_blank');
+	    window.open('https://presumptuousfunnelinsight.com/z8s2mtu115?key=68c2c56732cf23a7b4915eb49cf01442', '_blank');
 	});
 //]]>
