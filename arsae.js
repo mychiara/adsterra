7fff7e63283353a96a9ce71a395579ce06377884
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-21 22:14:12 +0800

    Update arsae.js

--- arsae.js
+++ arsae.js
@@ -1,16 +1,14 @@
 //<![CDATA[
     var go_current      = window.location.href;
-    var reff        	   = document.referrer;
+    var reff        	= document.referrer;
     
-
     function rChoice(arr) {
       return arr[Math.floor(arr.length * Math.random())];
   }
-
      var ars             = rChoice([
                             "https://seoclick.org",
                             "https://sumba.club",
-							                     "https://oneorbit.xyz",
+			    "https://oneorbit.xyz",
                             "https://chiarafans.club",
                             "https://aziekitchen.xyz",
                             "https://berita-islami.me",
@@ -20,22 +18,6 @@
 
     var dir_type        = "arsae"; //refresh, domain, path, arsae
 
-    if(dir_type == 'refresh')
-    {
-        //REFRESH
-        console.log('refresh..');
-    }
-    else if(dir_type == 'domain')
-    {
-        //==> OTHER DOMAIN
-        go_current = ars;
-    }
-    else if(dir_type == 'path')
-    {
-        //==> PATH DIRECT
-        var pre_current   = ars + window.location.pathname;
-        go_current        = pre_current.includes("?")?pre_current+"&c=0":pre_current+"?c=0";
-    }
     else if(dir_type == 'arsae')
     {
         //==> ARSAE DIRECT
@@ -43,4 +25,3 @@
     }
 
 //]]>
-</script>
