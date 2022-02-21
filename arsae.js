99f8983edc5d7af51dd059f4ba34689cad0dd2ea
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-21 22:28:29 +0800

    Update arsae.js

--- arsae.js
+++ arsae.js
@@ -1,29 +1,15 @@
-//<![CDATA[
+ //<![CDATA[
     if(document.referrer)
     {
-        var cek = document.referrer;
+    	var cek = document.referrer;
 
-        var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('yandex.') || cek.includes('facebook.');
+    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('yandex.') || cek.includes('facebook.');
 
       if(is_se)
-        var ars             = rChoice([
-                            "https://seoclick.org",
-                            "https://sumba.club",
-                            "https://oneorbit.xyz",
-                            "https://chiarafans.club",
-                            "https://aziekitchen.xyz",
-                            "https://berita-islami.me",
-                        ]);
-                       
-
-
-    var dir_type        = "arsae"; //refresh, domain, path, arsae
-
-    else if(dir_type == 'arsae')
-    {
-        //==> ARSAE DIRECT
-        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
-    }
+    	{
+    		var url = window.location.href;
+    		window.location = "https://www.berita-islami.me/?arsae="+ encodeURIComponent(url);
+    	}
     }
 //]]>
  
