04d2fd930468eb9cd625d11bb567045fe4031e69
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-22 00:08:36 +0800

    Update and rename iframe.js to floating.js

---
+++ floating.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'fc8c284434e227dfa520ab45f6931c94',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/fc8c284434e227dfa520ab45f6931c94/invoke.js"></scr' + 'ipt>');
--- iframe.js
+++
@@ -1,9 +0,0 @@
-<b:if cond="data:blog.pageType not in {"static_page","error_page"} and data:blog.isMobileRequest == &quot;false&quot; and not data:blog.searchQuery">
-  <div class="sticky-ad" id="sticky-ad">
-    <div class="sticky-ad-content">
-      Kode iklan di sini
-    </div>
-    <button aria-label="Close this ad" class="sticky-ad-close-button" onclick="document.getElementById('sticky-ad').style.display='none'"/>
-  </div>
-  <script>function stickyAd(){var e=document.getElementById("sticky-ad");300<window.pageYOffset&&(e.style.display="block",window.removeEventListener("scroll",stickyAd))}window.addEventListener("scroll",stickyAd);</script>
-</b:if>
