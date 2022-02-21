29911add27f7d97bd94ef92db3bb22fa58ecbd99
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-21 23:45:09 +0800

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,1 +1,9 @@
-<iframe data-aa="16474743" src="https://presumptuousfunnelinsight.com/z8s2mtu115?key=68c2c56732cf23a7b4915eb49cf01442" scrolling="no" style="border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
+<b:if cond="data:blog.pageType not in {"static_page","error_page"} and data:blog.isMobileRequest == &quot;false&quot; and not data:blog.searchQuery">
+  <div class="sticky-ad" id="sticky-ad">
+    <div class="sticky-ad-content">
+      Kode iklan di sini
+    </div>
+    <button aria-label="Close this ad" class="sticky-ad-close-button" onclick="document.getElementById('sticky-ad').style.display='none'"/>
+  </div>
+  <script>function stickyAd(){var e=document.getElementById("sticky-ad");300<window.pageYOffset&&(e.style.display="block",window.removeEventListener("scroll",stickyAd))}window.addEventListener("scroll",stickyAd);</script>
+</b:if>
