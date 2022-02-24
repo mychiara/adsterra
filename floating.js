a8f5a92e816e37835cfbe3b24c62edf3c9aabf40
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-24 09:39:55 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,11 +1,2 @@
-	atOptions = {
-		'key' : 'fc8c284434e227dfa520ab45f6931c94',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/fc8c284434e227dfa520ab45f6931c94/invoke.js"></scr' + 'ipt>');
-
 &lt;script async=&quot;async&quot; data-cfasync=&quot;false&quot; src=&quot;//racingorchestra.com/0915461b3d0cb2aa15d38e2ed7c336c3/invoke.js&quot;&gt;&lt;/script&gt;
 &lt;div id=&quot;container-0915461b3d0cb2aa15d38e2ed7c336c3&quot;&gt;&lt;/div&gt;
