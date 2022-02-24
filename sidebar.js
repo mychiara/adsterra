65b633d179ac087f301f720b32f6167dd24593ce
mychiara <85479169+mychiara@users.noreply.github.com>
2022-02-24 09:23:05 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,2 +1,2 @@
-<script async="async" data-cfasync="false" src="//racingorchestra.com/0915461b3d0cb2aa15d38e2ed7c336c3/invoke.js"></script>
+async="async" data-cfasync="false" src="//racingorchestra.com/0915461b3d0cb2aa15d38e2ed7c336c3/invoke.js">
 <div id="container-0915461b3d0cb2aa15d38e2ed7c336c3"></div>
