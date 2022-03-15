e0df77e6f6a5e8720949b812b5259dd4440f287e
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-15 13:37:21 +0800

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,6 +8,6 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://shp.ee/ijpipg3', '_blank');
+	    window.open('https://shp.ee/t87cpw3','https://shp.ee/jxt5g43','https://shp.ee/n5g8nw3', '_blank');
 	});
 //]]>
