9926bcd5c3007149c9c60e3dba39776fc9af486e
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-16 14:13:59 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,8 +1,12 @@
+&lt;script type=&#039;text/javascript&#039; src=&#039;//pl15692252.effectivecpmcontent.com/a6/ec/6c/a6ec6c0656aed056efe577111f430085.js&#039;&gt;&lt;/script&gt;
+
+&lt;script type=&quot;text/javascript&quot;&gt;
 	atOptions = {
-		'key' : '887d1ebe7501f6544975e832f7527848',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
+		&#039;key&#039; : &#039;887d1ebe7501f6544975e832f7527848&#039;,
+		&#039;format&#039; : &#039;iframe&#039;,
+		&#039;height&#039; : 250,
+		&#039;width&#039; : 300,
+		&#039;params&#039; : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/887d1ebe7501f6544975e832f7527848/invoke.js"></scr' + 'ipt>');
+	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://www.highconvertingformats.com/887d1ebe7501f6544975e832f7527848/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
+&lt;/script&gt;
