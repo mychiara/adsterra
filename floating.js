9b441e63298dbcf5814bdd3b19e9cc1a52b3adc5
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-16 14:29:51 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -6,5 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/887d1ebe7501f6544975e832f7527848/invoke.js"></scr' + 'ipt>');
-
-type='text/javascript' src='//pl15692252.effectivecpmcontent.com/a6/ec/6c/a6ec6c0656aed056efe577111f430085.js'>
