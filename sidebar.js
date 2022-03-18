4969ba7d29c06705b5e49430425e3e32a37458c0
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-18 15:38:41 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '0b9b13af0fa65c2bd68df74a86f15e37',
+		'key' : 'c63dc96d2e44a7f9360a5403f1949caf',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 300,
+		'width' : 160,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://racingorchestra.com/0b9b13af0fa65c2bd68df74a86f15e37/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/c63dc96d2e44a7f9360a5403f1949caf/invoke.js"></scr' + 'ipt>');
