f2131008ad033007d55d77631d7a5205adf50b39
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-26 23:44:03 +0800

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,6 +8,6 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://shp.ee/ijpipg3', '_blank');
+	    window.open('https://shope.ee/9oyIrbrJTc','https://shope.ee/9eesfIrwob','https://shope.ee/9ULSSzsa9a','https://shope.ee/9K22GgtDUZ','https://shope.ee/AUDzepom7o','https://shope.ee/AJuZSWpPSn','https://shope.ee/A9b9GDq2nm','https://shope.ee/9zHj3uqg8l','https://shope.ee/8USvH9wOBU','https://shope.ee/8K9V4qx1WT','https://shope.ee/89q4sXxerS','https://shope.ee/7zWegEyICR','https://shope.ee/99ic4Ntqpg','https://shope.ee/8zPBs4uUAf','https://shope.ee/8p5lflv7Ve','https://shope.ee/8emLTSvkqd','https://shope.ee/79xXgi1StM','https://shope.ee/6ze7UP26EL','https://shope.ee/6pKhI62jZK','https://shope.ee/6f1H5n3MuJ','https://shope.ee/7pDETvyvXY','https://shope.ee/7etoHczYsX','https://shope.ee/5ACPPDg75Q','_blank');
 	});
 //]]>
