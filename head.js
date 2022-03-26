953437b720db6d3dcb87958dce9b7c892c81a759
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-26 23:27:27 +0800

    Update head.js

--- head.js
+++ head.js
@@ -1,2 +1,1 @@
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl16176169.safestgatetocontent.com/1cdcac7d654c1999aa7295ebea633aff/invoke.js"></scr' + 'ipt>');
-<div id="container-1cdcac7d654c1999aa7295ebea633aff"></div>
+
