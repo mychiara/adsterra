665cac542cdf5ae2fc7960c5a7e6bd77f7f90fb8
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-26 23:36:24 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,9 +1,8 @@
-atOptions = {
-		'key' : '0c36e62040e13f23ed831c26d97931ec',
+	atOptions = {
+		'key' : 'fc8c284434e227dfa520ab45f6931c94',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 60,
+		'width' : 468,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/0c36e62040e13f23ed831c26d97931ec/invoke.js"></scr' + 'ipt>');
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl16775351.safestgatetocontent.com/bd/76/b3/bd76b3a021aad8533d2ad1a984f10dcf.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/fc8c284434e227dfa520ab45f6931c94/invoke.js"></scr' + 'ipt>');
