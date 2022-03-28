171291dac50d84ed58dbeb4f4e5521394c7b33f9
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-28 20:20:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : 'fc8c284434e227dfa520ab45f6931c94',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/fc8c284434e227dfa520ab45f6931c94/invoke.js"></scr' + 'ipt>');
+
