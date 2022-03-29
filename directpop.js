fc727e382535729bce85d6a5b7c76cccebe80b8b
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-29 22:57:03 +0800

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -11,5 +11,7 @@ type='text/javascript'>
 	    window.open('https://shope.ee/6Uhmz5pJ0c','_blank');
 	});
 document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15692252.safestgatetocontent.com/a6/ec/6c/a6ec6c0656aed056efe577111f430085.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15139236.safestgatetocontent.com/41/07/05/410705d22ad2cf0ef08f17bcc337d242.js"></scr' + 'ipt>');
+//]]>
 //]]>
 
