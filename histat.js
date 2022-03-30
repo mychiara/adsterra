b9968bdb2eea330e378bcf035206e2931348a08f
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-30 09:16:35 +0800

    Update histat.js

--- histat.js
+++ histat.js
@@ -1,7 +1,7 @@
-<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+   <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
 <!-- Histats.com  START  (aync)-->
 type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4588598,4,1,120,40,00010000']);
+_Hasync.push(['Histats.start', '1,4456329,4,5,172,25,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -9,5 +9,5 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
-<noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?4588598&amp;101" alt="" border="0" /></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4456329&101" alt="" border="0"></a></noscript>
 <!-- Histats.com  END  -->
