57286bb107e58fb064eb3c599ee782e37c28eee8
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-30 10:03:47 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,1 @@
-type='text/javascript'>
-//<![CDATA[
 
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15139236.safestgatetocontent.com/41/07/05/410705d22ad2cf0ef08f17bcc337d242.js"></scr' + 'ipt>');
-
-//]]>
