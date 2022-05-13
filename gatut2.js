ad6ec94217d9781cea6b4bc7087e7e22632724c4
mychiara <85479169+mychiara@users.noreply.github.com>
2022-05-13 21:48:19 +0800

    Create gatut2.js

---
+++ gatut2.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f194948d470ed44734b74edfe3edbabe',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/f194948d470ed44734b74edfe3edbabe/invoke.js"></scr' + 'ipt>');
