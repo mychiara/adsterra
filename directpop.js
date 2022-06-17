99f96d89e4abaa0a5169e6cfb8be358d6aee5ef2
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-17 22:38:07 +0800

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,7 +8,7 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://shope.ee/6Uhmz5pJ0c','_blank');
+	    window.open('https://www.profitablecpmgate.com/gdky8p1x7?key=a221c14e8540f1849547e770dcd9d5e2','_blank');
 	});
 //]]>
 
