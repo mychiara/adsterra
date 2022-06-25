53c039050257b33420674328bf074d3f989ab17f
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-26 00:13:16 +0800

    Update metatag1.js

--- metatag1.js
+++ metatag1.js
@@ -1,1 +1,4 @@
-
+var meta = document.createElement('meta');
+meta.setAttribute('name', 'google-site-verification');
+meta.setAttribute('content', 'iwUey-2sWNicmRKe8zLVTcM0CeMtP3r3GjlpqJDIE3Y');
+document.getElementsByTagName('head')[0].appendChild(meta);
