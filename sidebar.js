471d03ced9306c87af11a7c4d455c51b8bc933bb
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-25 22:11:51 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,1 @@
-atOptions = {
-		'height' : 150,
-		'width' : 150,
-		'params' : {}
-	};
-	src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZ6tYdVyDD-p9YLBu07aX-bB8b5Rve-vIqDjU8IWJyPPQjFE90AJGMQOiuRu8xAWp83Iy5s1D_QtGMgH1rGie6pw-lUcxkHUpP3cLVrO2G8Ox386a-KLR3JzRD1cFAcQS-PtBc8auu1BvjSSNSVKPB0q5m0NvKYhl1R4mGsDDmiG7FMXQ7yUZoCoDe/s500/61cd5b55287d5776157e2ea810f524b5.png'/<a title="Shopee Cuci Gudang" href='https://oneorbit.xyz' target='_blank'><img alt='Promo Shopee' ></a>
+&lt;center&gt;&lt;a title=&quot;Shopee Cuci Gudang&quot; href=&#039;https://oneorbit.xyz&#039; target=&#039;_blank&#039;&gt;&lt;img alt=&#039;Promo Shopee&#039; src=&#039;https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi4u5EpBQsNAPIWVYHjemZTrTgGHiq89LPCx_eUW4s602I8Zf_m8XuGi5XOjjwjnccFZ3YR8HHuySLuc0MFYa2SISssJqnrDGEYtrBoFYoGGTAqWEDsgNkHk6hH00WeljPKM1lr4Nr2izKXf69kFQTkMn3tyNSRBzuOlkAJnTlYBdR2bD08DK0YbfBb/s250/61cd5b55287d5776157e2ea810f524b5.png&#039; /&gt;&lt;/a&gt;&lt;/center&gt;
