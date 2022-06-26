e511f576ab0d3d154874e7ced59b1f0eda5b0132
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-26 22:28:54 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -4,3 +4,13 @@ document.write("<a href=\"https://shope.ee/6Uhmz5pJ0c\">");
 document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi4u5EpBQsNAPIWVYHjemZTrTgGHiq89LPCx_eUW4s602I8Zf_m8XuGi5XOjjwjnccFZ3YR8HHuySLuc0MFYa2SISssJqnrDGEYtrBoFYoGGTAqWEDsgNkHk6hH00WeljPKM1lr4Nr2izKXf69kFQTkMn3tyNSRBzuOlkAJnTlYBdR2bD08DK0YbfBb/s250/61cd5b55287d5776157e2ea810f524b5.png' />");
 document.write("</a>");
 //-->
+
+	atOptions = {
+		'key' : '7ceb09812c5d7cf8fb4711ff019320d1',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/7ceb09812c5d7cf8fb4711ff019320d1/invoke.js"></scr' + 'ipt>');
+
