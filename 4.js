6c2899512a2efee8f6836b8e50e94301d04cbea8
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-30 00:54:53 +1000

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,4 +1,2 @@
-type='text/javascript'>
-//<![CDATA[
-document.write type='text/javascript' src='//pl15293407.profitablecpmgate.com/61/e2/aa/61e2aa872807906eb359f576da2a3935.js'></script>
-//]]>
+language="text/javascript">
+  //pl16122820.profitablecpmgate.com/d6/80/f1/d680f186b549742f28e078126c2d7e93.js
