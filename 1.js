ec9375a843d58dc402e59306f83a7fd9589202f9
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-30 02:13:18 +1000

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,13 @@
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
 
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://www.profitablecpmgate.com/gdky8p1x7?key=a221c14e8540f1849547e770dcd9d5e2','_blank');
+	});
+//]]>
