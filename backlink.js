ec020142af6f83ec9515b7849abd9be6612701f0
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-29 21:26:58 +1000

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
 document.write("<a href=\"https://oneorbit.xyz/the-best-way-to-get-your-life-insurance-license\">");
-document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjkjJx6aG50ZxcYp0Bj1dNgNjLGmz5xRzUtXuS-kwUDnkvh9qhqtnBFXw7e0wwy18xRdaICc3eyyZ0KeKIYBOPzuLMRMqS2nde3LxeGNhvgHB-YkSx4ytCYxfRxOjjKusVk_P7rmyX0NfksVJE5gEQn-PjT2j68RD80GQp5OupSi4r2gf7OsrQz0qd8/s621/Screenshot_10.png' />");
+document.write("Read : Best Life Insurance Product");
 document.write("</a>");
 //-->
