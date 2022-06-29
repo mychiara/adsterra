8c420c0684cb65411462280bbad621640da1552d
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-30 00:03:26 +1000

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,2 +1,1 @@
-type="text/javascript">
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl16122820.profitablecpmgate.com/d6/80/f1/d680f186b549742f28e078126c2d7e93.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl16122820.profitablecpmgate.com/d6/80/f1/d680f186b549742f28e078126c2d7e93.js"></scr' + 'ipt>');
