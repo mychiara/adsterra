c196b93a4bbc6166fe945ac09205d183da71bf9e
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-29 23:30:14 +1000

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -5,12 +5,3 @@ document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/A
 document.write("</a>");
 //-->
 
-	atOptions = {
-		'key' : '7ceb09812c5d7cf8fb4711ff019320d1',
-		'format' : 'iframe',
-		'height' : 600,
-		'width' : 160,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/7ceb09812c5d7cf8fb4711ff019320d1/invoke.js"></scr' + 'ipt>');
-
