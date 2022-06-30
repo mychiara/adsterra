655cb92215794f0c13b83d7fc41065346f8c8f21
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-01 01:50:08 +1000

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
-document.write("<a href=\"https://oneorbit.xyz">");
+document.write("<a href=\"https://oneorbit.xyz/">");
 document.write("<b>Read : Best Life Insurance Product</b>");
 document.write("</a>");
 //-->
