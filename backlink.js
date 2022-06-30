4627c4a1a3cd4e46de28f8c91010040138061c57
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-01 01:54:21 +1000

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
 document.write("<a href=\"https://oneorbit.xyz/10-things-you-could-have-in-common-with-tax-attorney/">");
-document.write("<b>Read : Best Life Insurance Product</b>");
+document.write("<b>Read : Best Life Insurance Product</b>' />");
 document.write("</a>");
 //-->
