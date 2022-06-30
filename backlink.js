c19d72b139618d166e388a30353ffd33499c81b5
mychiara <85479169+mychiara@users.noreply.github.com>
2022-06-30 12:04:10 +0800

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
-document.write("<a href=\"https://oneorbit.xyz/the-best-way-to-get-your-life-insurance-license\">");
+document.write("<a href=\"https://oneorbit.xyz/">");
 document.write("<b>Read : Best Life Insurance Product</b>");
 document.write("</a>");
 //-->
