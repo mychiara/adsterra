8e665061ff348c036bea6239993a9a17e85b90d9
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-01 02:27:20 +1000

    Create shopeebackup.js

---
+++ shopeebackup.js
@@ -0,0 +1,6 @@
+language="javascript">
+<!--
+document.write("<a href=\"https://shope.ee/6Uhmz5pJ0c\">");
+document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi4u5EpBQsNAPIWVYHjemZTrTgGHiq89LPCx_eUW4s602I8Zf_m8XuGi5XOjjwjnccFZ3YR8HHuySLuc0MFYa2SISssJqnrDGEYtrBoFYoGGTAqWEDsgNkHk6hH00WeljPKM1lr4Nr2izKXf69kFQTkMn3tyNSRBzuOlkAJnTlYBdR2bD08DK0YbfBb/s250/61cd5b55287d5776157e2ea810f524b5.png' />");
+document.write("</a>");
+//-->
