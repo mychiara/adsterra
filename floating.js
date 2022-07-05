e4910886f5baae3d43fe0d7bae013bb027597766
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-05 22:53:02 +1000

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
 document.write("<a href=\"https://shope.ee/20I152QCWY\">");
-document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjKlVxV0aCkvncY4jON-G3BVke9p7wEtYI0RfyPIJmvJrOwMpfT_qe8MJx_PaYfVA75KSdcC7iY06d6GZdqOISMu5RueBHoqoW67Q1wPVK4BdrrPbhKrvaD54tG6GtaWvZwPQE9F8R7oT6OUvhBzk_-NZ8JL1PGUlqJ7jpK-rGxRCoRGZ2f3D6xOark/s600/Call%20me%20here.png' />");
+document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhZd2NBfwp-mDrVRbT41iY0E-PaD7vO6z4pG9063vKdzOOs7YMKM5RhTOUDiXlp3_Aaq_xYAzINtNmHE6zykxXaryhaXfWXPoXtdL6qpuPu8mEyqMvV_4Nbc2jBR9aVdHXQx5T22ejXOLAMTusI7sZvH5l_S1gTeiIcKffkBY5cTmp1zG6SRYNy8ls8/s600/Call%20me%20here.png' />");
 document.write("</a>");
 //-->
