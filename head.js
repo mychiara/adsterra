4900b16982a56811b90932ac44c1b6a80800ccc9
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-06 22:34:56 +1000

    Update head.js

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '5201db71584cd68b2c53675d013fff26',
+		'key' : 'aa7da29cf8563793fe21b629ba81c024',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 90,
+		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/5201db71584cd68b2c53675d013fff26/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/aa7da29cf8563793fe21b629ba81c024/invoke.js"></scr' + 'ipt>');
