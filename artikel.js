0c33a2ae090773175b27714dd815bcba748de786
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-06 22:35:19 +1000

    Update artikel.js

--- artikel.js
+++ artikel.js
@@ -1,9 +1,8 @@
 	atOptions = {
-		'key' : 'b58030d3a41df441bac7a51d56c143a7',
+		'key' : 'aa7da29cf8563793fe21b629ba81c024',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/b58030d3a41df441bac7a51d56c143a7/invoke.js"></scr' + 'ipt>');
-
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/aa7da29cf8563793fe21b629ba81c024/invoke.js"></scr' + 'ipt>');
