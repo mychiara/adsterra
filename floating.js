16274464035f1c86f6ba80c24a1a934d540becd5
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-19 23:36:31 +1000

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,4 +1,11 @@
-language="javascript">
-<!--
-ads/responsive.txt
-//-->
+async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6987669088940391"
+     crossorigin="anonymous"></script>
+<!-- bismillah -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6987669088940391"
+     data-ad-slot="8248336780"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
