661c32c4146a2e203e8d310ec6b59d8780382e46
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-19 23:23:19 +1000

    Create floatingbackup.js

---
+++ floatingbackup.js
@@ -0,0 +1,6 @@
+language="javascript">
+<!--
+document.write("<a href=\"https://www.profitablecpmgate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731\">");
+document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSg_JRRpw_ZDkiBoi4Q1J_HhON5XxmCpj0I5Y20gOxYFbKqblZ7j02TFLkv0eJ_KaxXULG4WOCNcs4zf9zpSM7aa7rN7hPTL8P3azEI9EknVQ5dCc_Fw6KACW4GKZscW8BT5SR0h-OHUFXhDQ32KPX6WJQYAL1hT8WQG_BIa3Ik8pyC0mpZSLK29Z5/s235/Call%20me%20here.png' />");
+document.write("</a>");
+//-->
