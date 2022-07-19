931a608633230ed526cf2c420f5f6ba0d8595be0
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-19 23:45:17 +1000

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,2 +1,3 @@
-type='text/javascript'> //<![CDATA[ { var greet = "<!--ads/responsive.txt-->";
-    document.write(greet); // Prints: <!--ads/responsive.txt--> } //]]>
+type='text/javascript'>
+    //<![CDATA[ { var greet = "<!--ads/responsive-->";
+    document.write(greet); // Prints: <!--ads/responsive--> } //]]>
