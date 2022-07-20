61782e2b2ab4a3439feec9ab850a21debc950815
mychiara <85479169+mychiara@users.noreply.github.com>
2022-07-20 20:37:10 +1000

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,1 +1,8 @@
-<!--ads/responsive.txt-->
+atOptions = {
+		'key' : '56671703daf8f709999a1a14a73647b5',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/56671703daf8f709999a1a14a73647b5/invoke.js"></scr' + 'ipt>');
