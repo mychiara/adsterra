2721bebaba040eb7831162c83ce0e560aec9d40f
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-04 09:19:33 +0800

    Update arsae.js

--- arsae.js
+++ arsae.js
@@ -1,19 +1,1 @@
-//<![CDATA[
-    if(document.referrer)
-    {
-    	var cek = document.referrer;
-
-    	var ars 	= rChoice([
-							'https://oneorbit.xyz/',
-							'https://insurance.oneorbit.xyz/',
-							]);
-
-				function rChoice(arr) {
-    			return arr[Math.floor(arr.length * Math.random())];
-	}
-
-	//Code arsae client javascript
-	if(['.google.', 'bing.', 'yandex.', 'duckduckgo.', 'facebook.', 'yahoo.', 'whatsapp.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-    }
-//]]>
 
