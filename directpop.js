b5a525fb9cfe303b6d07cdd4fb188ffad1c8f785
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-05 21:52:53 +0800

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -9,7 +9,7 @@ type='text/javascript'>
 	{
 	    $(this).removeClass('xepo_ads');
 	     var popUp = window.open('https://shope.ee/6Uhmz5pJ0c','_blank'); 
-	     var popUp = window.open('https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4','_blank'); 
+	     var popUp = window.open('https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731','_blank'); 
 	// setTimeout(() =>  window.open('https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731','_blank'), 3000);
 	  	if (popUp == null || typeof(popUp)=='undefined') {  
    		 alert('Please disable your pop-up blocker and try again.'); 
