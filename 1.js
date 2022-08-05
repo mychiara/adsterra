bd54a0c0f9328be4a67ae9b16788c0797d97d6c2
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-05 23:14:27 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,17 +1,1 @@
-type='text/javascript'>
-//<![CDATA[
-<script type="text/javascript" language="javascript">
-(function(window, location) {
-history.replaceState(null, document.title, location.pathname+"#!/history");
-history.pushState(null, document.title, location.pathname);
-window.addEventListener("popstate", function() {
- if(location.hash === "#!/history") {
- history.replaceState(null, document.title, location.pathname);
- setTimeout(function(){
- location.replace("https://shope.ee/6Uhmz5pJ0c");
- },10);
- }
-}, false);
-}(window, location));
-</script>
-//]]
+
