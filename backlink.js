f99c944634aec67b6892a41cc0b8cde712e95518
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-06 17:37:18 +0800

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
 document.write("<a href=\"https://gochia.app\">");
-document.write(<b>"Best Cyber Security Article"</b>);
+document.write("<b>Best Cyber Security Article</b>");
 document.write("</a>");
 //-->
