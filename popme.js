c266073a14b30ed13d754ad87ede08958289ca98
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-07 14:56:11 +0800

    Update popme.js

--- popme.js
+++ popme.js
@@ -10,7 +10,7 @@
                             "https://shope.ee/6Uhmz5pJ0c/",
                         ]);
 
-    var direct_link_ads = rChoice([
+    var direct_link_ads_1 = rChoice([
                             "https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731/",
                         ]);   
 
