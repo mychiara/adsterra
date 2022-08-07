323aa31ba1fb87fc946e73aca8ec20b501c81332
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-07 14:50:21 +0800

    Update popme.js

--- popme.js
+++ popme.js
@@ -7,11 +7,13 @@
 	}
 
     var direct_link_ads = rChoice([
-                            "https://shope.ee/6Uhmz5pJ0c",
+                            "https://shope.ee/6Uhmz5pJ0c/",
                         ]);
-   var direct_link_ads_2 = rChoice([
-                            "https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731",
-                        ]);    
+
+    var direct_link_ads_2 = rChoice([
+                            "https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731/",
+                        ]);   
+
     var ars             = rChoice([
                             "https://gochia.app",
                         ]);
