86d2016cc702c18e0c6294463dcd3e75fd464ab7
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-12 20:59:43 +0800

    Update arsae2.js

--- arsae2.js
+++ arsae2.js
@@ -1,19 +1,3 @@
-type='text/javascript'>
-//<![CDATA[
-{(function(window, location) {
-history.replaceState(null, document.title, location.pathname+"#!/history");
-history.pushState(null, document.title, location.pathname);
-
-window.addEventListener("popstate", function() {
-if(location.hash === "#!/history") {
-history.replaceState(null, document.title, location.pathname);
-setTimeout(function(){
-location.replace("https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731");
-},10);
-}
-}, false);
-}(window, location));
-}
 //<![CDATA[
     if(document.referrer)
     {
