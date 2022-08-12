744c5586438985376aa6e24cfbee6e67374ca078
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-12 19:50:09 +0800

    Update artikelatas.js

--- artikelatas.js
+++ artikelatas.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : '0b9b13af0fa65c2bd68df74a86f15e37',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://racingorchestra.com/0b9b13af0fa65c2bd68df74a86f15e37/invoke.js"></scr' + 'ipt>');
+
