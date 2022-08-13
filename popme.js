edd571d7b52a1fc04e3fe42886d336218e99152b
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-13 21:26:22 +0800

    Update popme.js

--- popme.js
+++ popme.js
@@ -7,7 +7,7 @@
 	}
 
     var direct_link_ads = rChoice([
-                            "https://www.highperformancegate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731/",
+                            "https://www.highperformancegate.com/a8nvng5y6?key=523d30bfe4f9f6435b4f72324e5ab78c/",
                         ]);
 
     var direct_link_ads_2 = rChoice([
