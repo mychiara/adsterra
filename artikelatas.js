60ac66c76695a62727b60e028fb78d90e9bb4f45
mychiara <85479169+mychiara@users.noreply.github.com>
2022-08-14 23:40:53 +0800

    Update artikelatas.js

--- artikelatas.js
+++ artikelatas.js
@@ -1,9 +1,1 @@
-	atOptions = {
-		'key' : 'b58030d3a41df441bac7a51d56c143a7',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/b58030d3a41df441bac7a51d56c143a7/invoke.js"></scr' + 'ipt>');
 
