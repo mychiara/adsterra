875a695a6ae8c5bbb4bf59ea374b4fb46b977cca
mychiara <85479169+mychiara@users.noreply.github.com>
2022-09-09 21:28:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/8e838fe86ab8194b2def61813e1a63f7/invoke.js"></scr' + 'ipt>');
-
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/8e838fe86ab8194b2def61813e1a63f7/invoke.js"></scr' + 'ipt>');
