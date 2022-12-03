59233b26a89859fa3944b1cb4b1439988aaf1831
mychiara <85479169+mychiara@users.noreply.github.com>
2022-12-03 23:58:13 +0800

    Create 720.js

---
+++ 720.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'd04b5e8712960b5564b30644da7c67ea',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/d04b5e8712960b5564b30644da7c67ea/invoke.js"></scr' + 'ipt>');
