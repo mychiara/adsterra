931268d7abdc6ec1ef2f8e9f6d74f9b02fac8ea7
mychiara <85479169+mychiara@users.noreply.github.com>
2022-12-04 00:32:48 +0800

    Create side.js

---
+++ side.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '7ceb09812c5d7cf8fb4711ff019320d1',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/7ceb09812c5d7cf8fb4711ff019320d1/invoke.js"></scr' + 'ipt>');
