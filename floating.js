6074111dfbc9faacaea200f2eee1af17d672e10b
mychiara <85479169+mychiara@users.noreply.github.com>
2023-01-05 11:44:36 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,6 +1,8 @@
-language="javascript">
-<!--
-document.write("<a href=\"https://www.profitablecpmgate.com/d7ha27e5?key=3e13d68a557a37d91e09eff64ec81731\">");
-document.write("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSg_JRRpw_ZDkiBoi4Q1J_HhON5XxmCpj0I5Y20gOxYFbKqblZ7j02TFLkv0eJ_KaxXULG4WOCNcs4zf9zpSM7aa7rN7hPTL8P3azEI9EknVQ5dCc_Fw6KACW4GKZscW8BT5SR0h-OHUFXhDQ32KPX6WJQYAL1hT8WQG_BIa3Ik8pyC0mpZSLK29Z5/s235/Call%20me%20here.png' />");
-document.write("</a>");
-//-->
+	atOptions = {
+		'key' : '8e838fe86ab8194b2def61813e1a63f7',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/8e838fe86ab8194b2def61813e1a63f7/invoke.js"></scr' + 'ipt>');
