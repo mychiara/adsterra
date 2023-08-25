b9e42a6590f1ab9b8c52cdd73942dec19c43a41f
mychiara <85479169+mychiara@users.noreply.github.com>
2023-08-25 08:36:44 +0800

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,6 @@
 language="javascript">
 <!--
-document.write("<a href=\"https://gochia.app\">");
+document.write("<a href=\"https://boscleine.com\">");
 document.write("<b>Best Techno Website</b>");
 document.write("</a>");
 //-->
