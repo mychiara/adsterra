c1f6c5aa57c5ddb2cecd37986a9df954fe0c8b0f
mychiara <85479169+mychiara@users.noreply.github.com>
2023-10-06 21:38:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+	atOptions = {
+		'key' : 'd04b5e8712960b5564b30644da7c67ea',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/d04b5e8712960b5564b30644da7c67ea/invoke.js"></scr' + 'ipt>');
 
