864d94e432362e9027ee32889ebb85ca2671a728
mychiara <85479169+mychiara@users.noreply.github.com>
2023-10-22 21:48:25 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,10 @@
+  // Redirect to the first URL in a new tab after 3 seconds
+  setTimeout(function() {
+    window.open('https://resepi-azhiekitchen.blogspot.com', '_blank');
+  }, 100); // 3000 milliseconds = 3 seconds
+
+  // Redirect to the second URL in a new tab after another 3 seconds
+  setTimeout(function() {
+    window.open('https://www.highcpmrevenuegate.com/exm0naa4xy?key=36009ef51061f3e7fe49ed079cab1be3', '_blank');
+  }, 1000); // 6000 milliseconds = 6 seconds
 
