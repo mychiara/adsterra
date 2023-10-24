7ed474ea77f2245121be2b3661031753eb5e24ed
mychiara <85479169+mychiara@users.noreply.github.com>
2023-10-24 23:14:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 	atOptions = {
-		'key' : 'd04b5e8712960b5564b30644da7c67ea',
+		'key' : '887d1ebe7501f6544975e832f7527848',
 		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
+		'height' : 250,
+		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/d04b5e8712960b5564b30644da7c67ea/invoke.js"></scr' + 'ipt>');
-
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/887d1ebe7501f6544975e832f7527848/invoke.js"></scr' + 'ipt>');
