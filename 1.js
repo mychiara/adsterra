9a67d1616aacd687cbb8720e5befab8e91ecb644
mychiara <85479169+mychiara@users.noreply.github.com>
2023-11-14 21:03:47 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
   // Redirect to the first URL in a new tab after 3 seconds
   setTimeout(function() {
-    window.open('https://boscleine.com', '_blank');
+    window.open('https://resepi-azhiekitchen.blogspot.com', '_blank');
   }, 100); // 3000 milliseconds = 3 seconds
 
   // Redirect to the second URL in a new tab after another 3 seconds
