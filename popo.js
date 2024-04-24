92a920ec971e265520d7524d05075594055b8668
mychiara <85479169+mychiara@users.noreply.github.com>
2024-04-24 22:17:00 +0800

    Update popo.js

--- popo.js
+++ popo.js
@@ -1,9 +1,9 @@
   // Redirect to the first URL in a new tab after 3 seconds
   setTimeout(function() {
-    window.open('https://resepi-azhiekitchen.blogspot.com', '_blank');
+    window.open('domaintarget', '_blank');
   }, 100); // 3000 milliseconds = 3 seconds
 
   // Redirect to the second URL in a new tab after another 3 seconds
   setTimeout(function() {
-    window.open('https://resepi.link/', '_blank');
+    window.open('directlink', '_blank');
   }, 1000); // 6000 milliseconds = 6 seconds
