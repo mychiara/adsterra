1f5b97e365aa0f00beeee72c46ec43b2c993675d
mychiara <85479169+mychiara@users.noreply.github.com>
2024-06-01 00:49:59 +0800

    Update floatings.js

--- floatings.js
+++ floatings.js
@@ -1,8 +1,8 @@
-	atOptions = {
+atOptions = {
 		'key' : '3438cc7f4724ab336b3dd4e9b68082c8',
 		'format' : 'iframe',
 		'height' : 600,
 		'width' : 160,
 		'params' : {}
 	};
-</script>
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/3438cc7f4724ab336b3dd4e9b68082c8/invoke.js"></scr' + 'ipt>');
