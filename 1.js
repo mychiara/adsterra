a39ab1828ccf8743a28acbcdafb0c24e9593f1f5
mychiara <85479169+mychiara@users.noreply.github.com>
2024-09-18 22:39:00 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,1 @@
-type='text/javascript'>
-//<![CDATA[
-	$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
 
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
-	    window.open('https://www.cpmrevenuegate.com/gye25cn3m3?key=41033cf68fd86d1ceb07ed75d18f04b6','_blank');
-	});
-//]]>
